1401e71b7cf30102342997f5b39c3f180dd64db6
linjianguang <1419479392@qq.com>
2026-06-30 09:03:16 +0800

    feat(Shimmer): add effect for text

--- packages/ui-components/src/components/Shimmer/index.tsx
+++ packages/ui-components/src/components/Shimmer/index.tsx
@@ -5,8 +5,29 @@ import { useEffect, useRef, type ReactNode } from "react";
 export type ShimmerProps = {
     className?: string;
     style?: React.CSSProperties;
-    color?: string;
     children: ReactNode;
+
+    /**
+     * shimmer 类型
+     * @default "text"
+     * @description shimmer 类型，可选值为 "text" 或 "container"
+     *
+     * "text" 是文本流光效果
+     * "container" 是容器流光效果
+     */
+    type?: "text" | "container";
+    /**
+     * shimmer 颜色
+     * @default "#ffffffff"
+     * @description shimmer 颜色，默认值为 "#ffffffff"
+     */
+    color?: string;
+    /**
+     * shimmer 持续时间
+     * @default 2
+     * @description shimmer 持续时间，默认值为 2 秒
+     */
+    duration?: number;
 };
 
 /**
@@ -17,6 +38,30 @@ export type ShimmerProps = {
 export const Shimmer = (props: ShimmerProps) => {
     const containerRef = useRef<HTMLDivElement>(null);
 
+    const {
+        className,
+        style,
+        children,
+        type = "text",
+        color = "#ffffffff",
+        duration = 2,
+    } = props;
+
+    useEffect(() => {
+        if (containerRef.current && color) {
+            containerRef.current.style.setProperty("--shimmer-color", color);
+        }
+    }, [color]);
+
+    useEffect(() => {
+        if (containerRef.current && typeof duration === "number") {
+            containerRef.current.style.setProperty(
+                "--shimmer-duration",
+                `${duration}s`,
+            );
+        }
+    }, [duration]);
+
     useEffect(() => {
         if (containerRef.current) {
             containerRef.current.style.setProperty(
@@ -24,16 +69,20 @@ export const Shimmer = (props: ShimmerProps) => {
                 `${containerRef.current?.clientWidth || 0}px`,
             );
         }
-    }, [props.children]);
+    }, [children]);
 
     return (
-        props.children && (
+        children && (
             <div
-                className={classNames(styles.shimmer, props.className)}
-                style={props.style}
+                className={classNames(
+                    styles.shimmer,
+                    styles[`shimmer-${type}`],
+                    className,
+                )}
+                style={style}
                 ref={containerRef}
             >
-                {props.children}
+                {children}
             </div>
         )
     );
